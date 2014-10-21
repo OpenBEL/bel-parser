bd78976a1bccce951a3f8658ffbe4268e30566eb
Anthony Bargnesi <abargnesi@gmail.com>
2014-10-21 10:35:22 -0400

    loop build on .c, .h, .rl, and Makefile files

--- tools/loop-build.sh
+++ tools/loop-build.sh
@@ -4,20 +4,19 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../
 if [ -z "$1" ]; then
     echo "usage: loop-build.sh" 1>&2
 fi
-RAGEL_FILE="$1"
 
 # loop compile + state chart
-inotifywait -r -m -e modify . | while read line; do
+inotifywait -m -e modify . | while read line; do
     RET=($line)
     FILE="${RET[@]:2}"
 
     case "$FILE" in
-        *.rl)
+        *.rl|*.h|term-parser.c|term-test.c|Makefile)
             RESULT=$(make clean all 2>&1 > /dev/null)
             if [ $? -ne 0 ]; then
                 LAST=$(echo -e "$RESULT" | tail -n1)
                 notify-send -t 2000 -u critical "Failed compilation\n\n$LAST"
-                echo "$RESULT" 2>&1
+                echo -e "$RESULT\n\n" 2>&1
             else
                 notify-send -t 2000 -u normal "Successful compilation"
             fi
