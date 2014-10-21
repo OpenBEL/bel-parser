ae4b9b351cacb058ce0f06b02facbd8b08806b4f
Anthony Bargnesi <abargnesi@gmail.com>
2014-10-21 08:31:46 -0400

    more tests; fixed assertions

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS=-ggdb -Wall
 RAGEL_OPTS=-G2 -L
 TEST_LIBS=-lcheck
 
-all:           clean test
+all:           clean main test
 
 ragel:
 	       ragel $(RAGEL_OPTS) term.rl -o term.c
@@ -14,7 +14,7 @@ main:          ragel
 
 test:          term.o tests.o
 	       gcc -o run-tests term.o tests.o $(TEST_LIBS)
-	       ./run-tests
+	       CK_VERBOSITY=verbose ./run-tests
 
 tests.o:
 	       checkmk *-test.c > tests.c
--- term-test.c
+++ term-test.c
@@ -2,15 +2,36 @@
 #include <string.h>
 #include "belparser.h"
 
-#test missing_namespace
+#test supports_parameters_without_namespace
     char line[BUFSIZE] = "p(AKT1)";
     char value[VALUE_SIZE];
     memset(value, '\0', VALUE_SIZE);
 
-    fprintf(stdout, "%s\n", bel_ast_flat_string(parse_term(line, value)));
+    fail_unless(
+            strcmp(
+                bel_ast_flat_string(parse_term(line, value)),
+                "TERM fx(p) ARG NV pfx() val(AKT1) ARG NIL NIL ") == 0,
+            "[parse_term] failed to parse parameter without namespace (e.g. AKT1)");
+
+#test supports_parameters_with_namespace
+    char line[BUFSIZE] = "p(HGNC:AKT1)";
+    char value[VALUE_SIZE];
+    memset(value, '\0', VALUE_SIZE);
 
     fail_unless(
             strcmp(
                 bel_ast_flat_string(parse_term(line, value)),
-                "TERM fx(p) ARG NV pfx() val(AKT1) ARG NIL NIL \n"),
-            "[parse_term] failed to parse terms missing namespace (e.g. p(AKT1))");
+                "TERM fx(p) ARG NV pfx(HGNC) val(AKT1) ARG NIL NIL ") == 0,
+            "[parse_term] failed to parse parameter with namespace (e.g. HGNC:AKT1)");
+
+#test supports_multiple_parameters
+    char line[BUFSIZE] = "p(AKT1, HGNC:AKT2, AKT3)";
+    char value[VALUE_SIZE];
+    memset(value, '\0', VALUE_SIZE);
+
+    fail_unless(
+            strcmp(
+                bel_ast_flat_string(parse_term(line, value)),
+                "TERM fx(p) ARG NV pfx() val(AKT1) ARG NV pfx(HGNC) val(AKT2) ARG NV pfx() val(AKT3) ARG NIL NIL ") == 0,
+            "[parse_term] failed to parse multiple parameters (e.g. AKT1, HGNC:AKT2, AKT3)");
+
