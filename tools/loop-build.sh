016b7206c3b53f278493f039b346e2388b5e71ec
Anthony Bargnesi <abargnesi@gmail.com>
2015-03-26 17:10:23 -0400

    rev'd loop-build forward to latest tree

--- tools/loop-build.sh
+++ tools/loop-build.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../
 
-if [ -z "$1" ]; then
-    echo "usage: loop-build.sh" 1>&2
-fi
-
 # loop compile + state chart
-inotifywait -m -e modify . | while read line; do
+inotifywait -m -e modify src tests | while read line; do
     RET=($line)
     FILE="${RET[@]:2}"
 
     case "$FILE" in
-        *.rl|*.h|bel-ast.c|bel-node-stack.c|bel-parser.c|bel-token.c|term-test.c|Makefile)
-            RESULT=$(make clean all 2>&1)
+        *.rl|*.h|*.tc|bel-ast.c|bel-node-stack.c|bel-parser.c|bel-token.c)
+            echo "Tripped build: $FILE"
+            RESULT=$(make all 2>&1)
             if [ $? -ne 0 ]; then
                 LAST=$(echo -e "$RESULT" | tail -n1)
                 notify-send -t 2000 -u critical "Failed compilation\n\n$LAST"
